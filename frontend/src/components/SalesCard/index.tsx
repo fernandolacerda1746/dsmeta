eb30ba7b9ffe0d525abe19bf95d11d46341bf14e
fernandolacerda1746 <stingeralfa@gmail.com>
2022-09-09 17:50:58 -0300

    Send Notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,8 +21,7 @@ function SalesCard() {
 
         const dmin = minDate.toISOString().slice(0, 10)
         const dmax = maxDate.toISOString().slice(0, 10)
-        console.log(dmin)
-        console.log(dmax)
+        
 
         axios.get(`${BASE_URL}/sales?MinDate=${dmin}&MaxDate=${dmax}`)
             .then(response => {
@@ -77,7 +76,7 @@ function SalesCard() {
                                     <td>{sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id}/>
                                         </div>
                                     </td>
                                 </tr>
